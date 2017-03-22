313e9665ebf384e609e22c324ba0c0f1a5694664
shawn2017 <shawn2016@aliyun.com>
2017-03-22 13:46:52 +0800

    sample app chapter 3.5 Done.

--- sample_app/test/controllers/static_pages_controller_test.rb
+++ sample_app/test/controllers/static_pages_controller_test.rb
@@ -5,19 +5,39 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   为了测试首页，向StaticPages 控制器中home 动作对应的
 URL 发起GET 请求，确认得到的是表示成功的响应码。
 =end
+
+  def setup
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+
+  # root
+  test "should get root" do
+    get static_pages_home_url
+    assert_response :success
+  end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
+    assert_select "title", "About | #{@base_title}"
+  end
+
+  test "should get contact" do
+    get static_pages_contact_url
+    assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
   end
 
 end
